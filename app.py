54133bae3e5723fd94b8fcd86c2e21824ae9867f
Kushal <134078114+Kushal112003@users.noreply.github.com>
2025-04-18 22:35:52 +0530

    Initial commit: Added project files

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, render_template
+import joblib
+
+app = Flask(__name__)
+
+# Load your trained model
+model = joblib.load('model.pkl')
+
+@app.route('/')
+def index():
+    return render_template('index.html', prediction="")
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    input_value = request.form['input_value']
+    prediction = model.predict([[float(input_value)]])
+    return render_template('index.html', prediction=prediction[0])
+
+if __name__ == '__main__':
+    app.run(debug=True)
