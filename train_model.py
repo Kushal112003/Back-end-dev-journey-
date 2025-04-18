84ed99fa5dc4715496b2eef08d78296c3013d224
Kushal <134078114+Kushal112003@users.noreply.github.com>
2025-04-18 22:56:16 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,16 +1,12 @@
 from sklearn.linear_model import LogisticRegression
 import joblib
 
-# 1. Sample data
-# X = hours studied, y = pass or fail
 X = [[1], [2], [3], [4], [5], [6], [7], [8], [9], [10]]
 y = [0, 0, 0, 0, 1, 1, 1, 1, 1, 1]  # 0 = fail, 1 = pass
 
-# 2. Create and train model
 model = LogisticRegression()
 model.fit(X, y)
 
-# 3. Save the trained model
 joblib.dump(model, 'model.pkl')
 
-print("✅ Model trained and saved as model.pkl")
+print("Model trained and saved as model.pkl")
